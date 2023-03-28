ef04afa88a6248e1fd1e37ef911875337b28151a
Festus Omaku <fomaku3@gmail.com>
2023-03-28 11:15:37 +0100

    crreate new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,9 +7,8 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *temp_node;
+	binary_tree_t *temp_node = malloc(sizeof(binary_tree_t));
 
-	*temp_node = malloc(sizeof(binary_tree_t));
 	if (temp_node == NULL)
 	{
 		return (NULL);
