d25c91c54c16de267a0b514efe5a2f8492440d88
Festus Omaku <fomaku3@gmail.com>
2023-03-29 15:40:19 +0100

    fixing the left node code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,7 +5,7 @@
  *@value: value of node
  *Return: left new node or null
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
