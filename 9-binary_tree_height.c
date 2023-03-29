71eff2d65859dca08fdbbc326c1c94b49383eb5a
Festus Omaku <fomaku3@gmail.com>
2023-03-29 23:45:39 +0100

    fixing an error

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	size_t leftHueight = binary_tree_height(tree->left);
+	size_t leftHeight = binary_tree_height(tree->left);
 	size_t rightHeight = binary_tree_height(tree->right);
 
 	if (leftHeight > rightHeight)
