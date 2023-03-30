94d3d074d385148c5f7909a13fc50e789a34c7fc
Festus Omaku <fomaku3@gmail.com>
2023-03-30 02:22:41 +0100

    measuring the size of binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,10 +6,11 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
+
 	size_t lefts = binary_tree_size(tree->left);
 	size_t rights = binary_tree_size(tree->right);
 
-	if (tree == NULL)
-		return (0);
 	return (1 + lefts + rights);
 }
