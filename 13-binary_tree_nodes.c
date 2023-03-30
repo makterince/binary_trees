1a922eb8ade57a4b377be8c462ea8ace5818c412
Festus Omaku <fomaku3@gmail.com>
2023-03-30 02:45:42 +0100

    counts the nodes that are not leaves

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_nodes - counts the node with at least one child
+ *@tree: pointer to nodes in binary tree
+ *Return: count
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL || tree->right != NULL)
+		return (1);
+	return (binary_tree_nodes(tree->left)
+			+ binary_tree_nodes(tree->right));
+}
